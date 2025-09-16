4412f9255eafe5cbe7f306c8a5fbdc94e71e2448
Hernan Barrientos <hrn.2211@gmail.com>
2025-09-16 00:39:11 -0300

    organizando para subir pra web FRONTEND

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000', // A URL da sua API FastAPI
+  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000',
 });
 
 export default api;
